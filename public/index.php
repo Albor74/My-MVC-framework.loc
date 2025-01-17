d4c32a14bf6940807a9935fa5af78957c2a04ad5
Albor <albor74@list.ru>
2025-01-17 15:21:32 +0300

    Создали файлы ядра для маршрутизации

--- public/index.php
+++ public/index.php
@@ -4,9 +4,13 @@ if (PHP_MAJOR_VERSION < 8.0){
 }
 
 require_once __DIR__ . '/../config/init.php';// подключаем файл с константами
-
-
 require_once ROOT . '/vendor/autoload.php';
 
 
-//new \PHPFramework\Test();
+$app = new \PHPFramework\Application();
+require_once CONFIG . '/routes.php';
+
+
+//dump($app->router->getRoutes());
+//
+//echo call_user_func($app->router->getRoutes()['POST']['/contact']);// т.е. мы обращаемся к нашеиу массиву GET
